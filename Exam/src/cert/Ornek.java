4d296627e954e727e12aa573f09bb7a68f7e7fbf
IS96307 <IS96307@T2LPRVT2639.gm.isbank.com.tr>
2021-06-21 09:01:37 +0300

    ornek dosya

---
+++ Exam/src/cert/Ornek.java
@@ -0,0 +1,25 @@
+package cert;
+
+import java.util.Arrays;
+
+public class Ornek {
+	public static void main(String[] args) {
+		int[] arr1={7,5,6,1,4,2};
+		Arrays.sort(arr1);
+		System.out.println(Arrays.toString(arr1));
+
+		/*2 sayı arasında ki fark 1 olmak zorunda*/
+	try {
+		for(int i=0; i<arr1.length; ++i) {
+				int min=arr1[i];
+				int max=arr1[i+1];
+				if(max-min!=1) {
+					 int sayı=max-1;
+					 System.out.println(sayı);
+				}
+		}
+		}catch(ArrayIndexOutOfBoundsException arrException) {
+			arrException.printStackTrace();
+		}
+	}
+}
